8a38096d1701e0112b2d44c1bd752d3f83836240
Seb <sebastian.schyberg@gmail.com>
2024-04-16 11:13:20 +0200

    mere sjov med ant

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import React from "react";
 import { Button } from "antd";
+import { Divider } from "antd";
+import { Tabs } from "antd";
 
 const App = () => (
   <div className="App">
+    <Button type="primary">Button</Button>
+    <Divider />
     <Button type="primary">Button</Button>
   </div>
 );
