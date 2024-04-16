bd46b403ae385636d09db105c2cc9e7b753a6cab
Seb <sebastian.schyberg@gmail.com>
2024-04-16 11:02:00 +0200

    forfraa

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import ArticleList from "./components/ArticleList";
-import ReactFlipCard from "@holbech/react-flip-card";
-import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import { useState } from "react";
+import React from "react";
+import { Button } from "antd";
 
-function App() {
-  const data = {
-    username: "mig",
-    email: "mig@me.com",
-    articles: [
-      { header: "Hello World", content: "This is my first article" },
-      { header: "Hello World 2", content: "This is my second article" },
-      { header: "Hello World 3", content: "This is my third article" },
-    ],
-    contactInfo: {
-      email: "stuff@kea.dk",
-    },
-  };
-  return (
-    <>
-      <Header username={data.username} email={data.email} />
-      <ArticleList articles={data.articles} />
-      <Footer email={data.contactInfo.email} />
-
-      <div style={{ "--width": "500px", "--height": "500px" }}>
-        <ReactFlipCard>
-          <p>Front</p>
-          <p>Back</p>
-        </ReactFlipCard>
-      </div>
-      <div>
-        <ClimbingBoxLoader color="#36d7b7" />
-        <ClimbingBoxLoader color="#36d7b7" />
-        <ClimbingBoxLoader color="#36d7b7" />
-        <ClimbingBoxLoader color="#36d7b7" />
-        <ClimbingBoxLoader color="#36d7b7" />
-      </div>
-    </>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
 
 export default App;
